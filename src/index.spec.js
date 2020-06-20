7e2b666b6dec9be3f45cf1ca94a89f29212cbb8a
foo <foo@bar.de>
2020-06-20 02:50:24 +0200

    chore: refactor tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,95 +1,82 @@
 import withLocalTmpDir from 'with-local-tmp-dir'
 import outputFiles from 'output-files'
-import { endent } from '@dword-design/functions'
+import { endent, mapValues } from '@dword-design/functions'
 import execa from 'execa'
 
+const runTest = files => () =>
+  withLocalTmpDir(async () => {
+    await outputFiles({
+      'depcheck.config.js': endent`
+    const execaDetector = require('../src')
+
+    module.exports = {
+      detectors: [
+        execaDetector,
+      ],
+    }
+  `,
+      ...files,
+    })
+    await execa.command('depcheck --config depcheck.config.js')
+  })
+
 export default {
-  'bin object': () =>
-    withLocalTmpDir(async () => {
-      await outputFiles({
-        'depcheck.config.js': endent`
-        const execaDetector = require('../src')
+  'bin object': {
+    'node_modules/foo/package.json': endent`
+      {
+        "bin": {
+          "bar": "./dist/cli.js"
+        }
+      }
+    `,
+    'package.json': endent`
+      {
+        "dependencies": {
+          "foo": "^1.0.0"
+        }
+      }
+    `,
+    'src/index.js': "execa('bar')",
+  },
+  'bin string': {
+    'node_modules/foo/package.json': endent`
+    {
+      "name": "foo",
+      "bin": "./dist/cli.js"
+    }
+  `,
+    'package.json': endent`
+    {
+      "dependencies": {
+        "foo": "^1.0.0"
+      }
+    }
+  `,
+    'src/index.js': "execa('foo')",
+  },
+  command: {
+    'depcheck.config.js': endent`
+    const execaDetector = require('../src')
 
-        module.exports = {
-          detectors: [
-            execaDetector,
-          ],
-        }
-      `,
-        'node_modules/foo/package.json': endent`
-        {
-          "bin": {
-            "bar": "./dist/cli.js"
-          }
-        }
-      `,
-        'package.json': endent`
-        {
-          "dependencies": {
-            "foo": "^1.0.0"
-          }
-        }
-      `,
-        'src/index.js': "execa('bar')",
-      })
-      await execa.command('depcheck --config depcheck.config.js')
-    }),
-  'bin string': () =>
-    withLocalTmpDir(async () => {
-      await outputFiles({
-        'depcheck.config.js': endent`
-        const execaDetector = require('../src')
-
-        module.exports = {
-          detectors: [
-            execaDetector,
-          ],
-        }
-      `,
-        'node_modules/foo/package.json': endent`
-        {
-          "name": "foo",
-          "bin": "./dist/cli.js"
-        }
-      `,
-        'package.json': endent`
-        {
-          "dependencies": {
-            "foo": "^1.0.0"
-          }
-        }
-      `,
-        'src/index.js': "execa('foo')",
-      })
-      await execa.command('depcheck --config depcheck.config.js')
-    }),
-  command: () =>
-    withLocalTmpDir(async () => {
-      await outputFiles({
-        'depcheck.config.js': endent`
-        const execaDetector = require('../src')
-
-        module.exports = {
-          detectors: [
-            execaDetector,
-          ],
-        }
-      `,
-        'node_modules/foo/package.json': endent`
-        {
-          "name": "foo",
-          "bin": "./dist/cli.js"
-        }
-      `,
-        'package.json': endent`
-        {
-          "dependencies": {
-            "foo": "^1.0.0"
-          }
-        }
-      `,
-        'src/index.js': "execa.command('foo bar')",
-      })
-      await execa.command('depcheck --config depcheck.config.js')
-    }),
-}
+    module.exports = {
+      detectors: [
+        execaDetector,
+      ],
+    }
+  `,
+    'node_modules/foo/package.json': endent`
+    {
+      "name": "foo",
+      "bin": "./dist/cli.js"
+    }
+  `,
+    'package.json': endent`
+    {
+      "dependencies": {
+        "foo": "^1.0.0"
+      }
+    }
+  `,
+    'src/index.js': "execa.command('foo bar')",
+  },
+} |> mapValues(runTest)
