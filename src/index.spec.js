7fcf5fb782c68674e03cd13c552820ebc34fc12d
Sebastian Landwehr <info@sebastianlandwehr.com>
2021-06-09 22:00:43 +0200

    fix: use official depcheck (#37)

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,132 +1,74 @@
-import { endent, mapValues } from '@dword-design/functions'
-import execa from 'execa'
+import { endent } from '@dword-design/functions'
+import tester from '@dword-design/tester'
+import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
+import depcheck from 'depcheck'
 import outputFiles from 'output-files'
-import withLocalTmpDir from 'with-local-tmp-dir'
 
-const runTest = files => () =>
-  withLocalTmpDir(async () => {
-    await outputFiles({
-      'depcheck.config.js': endent`
-    const execaDetector = require('../src')
+import self from '.'
 
-    module.exports = {
-      detectors: [
-        execaDetector,
-      ],
-    }
-  `,
-      ...files,
-    })
-    await execa.command('depcheck --config depcheck.config.js')
-  })
-
-export default {
-  'bin object': {
-    'node_modules/foo/package.json': endent`
-      {
-        "bin": {
-          "bar": "./dist/cli.js"
-        }
-      }
-    `,
-    'package.json': endent`
-      {
-        "dependencies": {
-          "foo": "^1.0.0"
-        }
-      }
-    `,
-    'src/index.js': "execa('bar')",
-  },
-  'bin string': {
-    'node_modules/foo/package.json': endent`
+export default tester(
+  {
+    'bin object': {
+      'node_modules/foo/package.json': JSON.stringify({
+        bin: {
+          bar: './dist/cli.js',
+        },
+      }),
+      'src/index.js': "execa('bar')",
+    },
+    'bin string': {
+      'node_modules/foo/package.json': endent`
     {
       "name": "foo",
       "bin": "./dist/cli.js"
     }
   `,
-    'package.json': endent`
-    {
-      "dependencies": {
-        "foo": "^1.0.0"
-      }
-    }
-  `,
-    'src/index.js': "execa('foo')",
-  },
-  command: {
-    'depcheck.config.js': endent`
-    const execaDetector = require('../src')
-
-    module.exports = {
-      detectors: [
-        execaDetector,
-      ],
-    }
-  `,
-    'node_modules/foo/package.json': endent`
+      'src/index.js': "execa('foo')",
+    },
+    command: {
+      'node_modules/foo/package.json': endent`
     {
       "name": "foo",
       "bin": "./dist/cli.js"
     }
   `,
-    'package.json': endent`
-    {
-      "dependencies": {
-        "foo": "^1.0.0"
-      }
-    }
-  `,
-    'src/index.js': "execa.command('foo bar')",
-  },
-  'template tag: params': {
-    'depcheck.config.js': endent`
-    const execaDetector = require('../src')
-
-    module.exports = {
-      detectors: [
-        execaDetector,
-      ],
-    }
-  `,
-    'node_modules/foo/package.json': endent`
+      'src/index.js': "execa.command('foo bar')",
+    },
+    'template tag: params': {
+      'node_modules/foo/package.json': endent`
     {
       "name": "foo",
       "bin": "./dist/cli.js"
     }
   `,
-    'package.json': endent`
-    {
-      "dependencies": {
-        "foo": "^1.0.0"
-      }
-    }
-  `,
-    'src/index.js': "execa.command(`foo bar ${'bar'}`)",
-  },
-  'template tag: simple': {
-    'depcheck.config.js': endent`
-    const execaDetector = require('../src')
-
-    module.exports = {
-      detectors: [
-        execaDetector,
-      ],
-    }
-  `,
-    'node_modules/foo/package.json': endent`
+      'src/index.js': "execa.command(`foo bar ${'bar'}`)",
+    },
+    'template tag: simple': {
+      'node_modules/foo/package.json': endent`
     {
       "name": "foo",
       "bin": "./dist/cli.js"
     }
   `,
-    'package.json': endent`
-    {
-      "dependencies": {
-        "foo": "^1.0.0"
-      }
-    }
-  `,
-    'src/index.js': "execa.command(`foo ${'bar'} baz`)",
+      'src/index.js': "execa.command(`foo ${'bar'} baz`)",
+    },
   },
-} |> mapValues(runTest)
+  [
+    {
+      transform: test => async () => {
+        await outputFiles(test)
+
+        const result = await depcheck('.', {
+          detectors: [self],
+          package: {
+            dependencies: {
+              foo: '^1.0.0',
+            },
+          },
+        })
+        expect(result.dependencies).toEqual([])
+      },
+    },
+    testerPluginTmpDir(),
+  ]
+)
